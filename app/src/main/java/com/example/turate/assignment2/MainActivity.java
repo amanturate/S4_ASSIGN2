d03f15eaa5e1dc259b27550261466c9fe467d3b8
Aman Turate <turateaman1995@gmail.com>
2017-07-01 12:07:28 +0530

    Updated App

--- app/src/main/java/com/example/turate/assignment2/MainActivity.java
+++ app/src/main/java/com/example/turate/assignment2/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.turate.assignment2;
 
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         mlistview = (ListView) findViewById(R.id.listview);
 
@@ -46,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        }
-    }
+        }}
+
+
 
